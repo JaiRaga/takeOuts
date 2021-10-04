8e5b47f09bf43f2713a9cbccef754e534f092a50
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-04 15:30:19 +0530

    :sparkles: added restaurant screen component to main page and added status bar

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+
+import RestaurantsScreen from './src/features/restaurants/screens/restaurants.screen'
 
 export default function App() {
 	return (
 		<>
-			<SafeAreaView style={styles.container}>
-				<View style={styles.search}>
-					<Text>Search</Text>
-				</View>
-				<View style={styles.list}>
-					<Text>List</Text>
-				</View>
-			</SafeAreaView>
-			<StatusBar style='light' />
+			<RestaurantsScreen />
+			<ExpoStatusBar style='dark' />
 		</>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#aaa',
-	},
-	search: {
-		padding: 16,
-		backgroundColor: '#04f',
-	},
-	list: {
-		flex: 1,
-		padding: 16,
-		backgroundColor: '#915',
-	},
-})
