a1660ec8ec7c9dee8b92b7e66d4d2153f81a418d
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-04 15:28:12 +0530

    :sparkles: used styled components for cosistent styles, added safe area and search bar component

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { StatusBar, SafeAreaView, View } from 'react-native'
+import { Searchbar } from 'react-native-paper'
+
+import RestaurantInfoCard from '../components/restaurant-info-card.component'
+
+const SafeArea = styled(SafeAreaView)`
+	flex: 1;
+	margin-top: ${StatusBar.currentHeight}px;
+`
+
+const SearchContainer = styled.View`
+	padding: 16px;
+`
+
+const RestaurantListContainer = styled(View)`
+	flex: 1;
+	padding: 16px;
+	background-color: #915;
+`
+
+const RestaurantsScreen = () => {
+	return (
+		<SafeArea>
+			<SearchContainer>
+				<Searchbar />
+			</SearchContainer>
+			<RestaurantListContainer>
+				<RestaurantInfoCard />
+			</RestaurantListContainer>
+		</SafeArea>
+	)
+}
+
+export default RestaurantsScreen
