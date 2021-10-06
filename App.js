7adf25fd65d39e37065dc2d45f8243224a43de1c
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-06 08:44:08 +0530

    :zap: passed theme file to child components through theme provider from styled components

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components/native'
 
+import { theme } from './src/infrastructure/theme'
 import RestaurantsScreen from './src/features/restaurants/screens/restaurants.screen'
 
 export default function App() {
 	return (
 		<>
-			<RestaurantsScreen />
-			<ExpoStatusBar style='dark' />
+			<ThemeProvider theme={theme}>
+				<RestaurantsScreen />
+				<ExpoStatusBar style='dark' />
+			</ThemeProvider>
 		</>
 	)
 }
